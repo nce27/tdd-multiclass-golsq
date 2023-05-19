7fffa1a6cf556984b676bd2fcad4d4eee13fb6ef
nce27 <nc3elliott@gmail.com>
2023-05-19 18:08:06 +0100

    Updated the integration spec for adding multiple entries.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe "integration" do
   context "after adding some diary entries" do
     it "lists the entries added" do
       diary = Diary.new
-      diary_entry = DiaryEntry.new("my_title", "my_contents")
-      diary.add(diary_entry)
-      expect(diary.all).to eq [diary_entry]
+      diary_entry_1 = DiaryEntry.new("my_title", "my_contents")
+      diary_entry_2 = DiaryEntry.new("my_title 2", "my_contents 2")
+      diary.add(diary_entry_1)
+      diary.add(diary_entry_2)
+      expect(diary.all).to eq [diary_entry_1, diary_entry_2] 
     end
   end
 end
