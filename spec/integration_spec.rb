4eaad106ff7ac429cb9e568a7385ba6049677e44
nce27 <nc3elliott@gmail.com>
2023-05-19 18:43:08 +0100

    Updated integration spec file to count all the words in the diary entries.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe "integration" do
       diary.add(diary_entry_2)
       expect(diary.all).to eq [diary_entry_1, diary_entry_2] 
     end
+    describe "#count_words" do
+      xit "counts the words in the entries' contents" do
+        diary = Diary.new
+        diary_entry_1 = DiaryEntry.new("my_title", "my_contents")
+        diary_entry_2 = DiaryEntry.new("my_title 2", "my_contents 2")
+        diary.add(diary_entry_1)
+        diary.add(diary_entry_2)
+        expect(diary.count_words).to eq 5
+      end
+    end
   end
 end
