a56c6866f07eafd33556fed095261dfa2d835b8d
nce27 <nc3elliott@gmail.com>
2023-05-19 18:41:38 +0100

    Updated diary entry spec file to construct a diary entry with title and contents.

---
+++ spec/diary_entry_spec.rb
@@ -0,0 +1,12 @@
+require "diary_entry"
+
+RSpec.describe DiaryEntry do
+  context "" do
+    it "constructs" do
+      diary_entry = DiaryEntry.new("my_title", "my_contents")
+      expect(diary_entry.title).to eq "my_title"
+      expect(diary_entry.contents).to eq "my_contents"
+    end
+
+  end
+end
