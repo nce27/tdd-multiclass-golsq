0c06260e6fba48833134350d51dc61efb3d558b1
nce27 <nc3elliott@gmail.com>
2023-05-19 18:53:11 +0100

    Updated the diary entry spec file to count the words in the contents.

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe DiaryEntry do
       expect(diary_entry.title).to eq "my_title"
       expect(diary_entry.contents).to eq "my_contents"
     end
+  end
 
+  describe "#count_words" do
+    it "counts the words in the contents" do
+      diary_entry = DiaryEntry.new("my_title", "one, two, three, four")
+      expect(diary_entry.count_words).to eq 4
+    end
   end
 end
